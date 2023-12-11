5bbfe750b102fe0124b6b7694b09bc21184c4687
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2023-12-11 14:31:37 +0000

    Adding functions to ApplicationController from previous code

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,8 +1,8 @@
 package controllers
 
 import connectors.GitHubConnector
-import models.UserModel
-import play.api.libs.json.Json
+import models.{DataModel, UserModel}
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.Results._
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import services.RepositoryService
@@ -25,21 +25,31 @@ class ApplicationController @Inject()(
     }
   }
 
-//  def displayUser(name: String): Action[AnyContent] = Action.async { implicit request =>
-//    repositoryService.read(name).map {
-//      case Right(user: UserModel) => Ok(views.html.displayuser(user))
-//      case Left(err: String) =>
-//        BadRequest(
-//          views.html.displayuser(
-//            UserModel(login = "404",
-//                      id = 404,
-//                      url = "404",
-//                      node_id = "404",
-//                      created_at = "404",
-//                      followers = 404,
-//                      following = 404)
-//          )
-//        )
-//    }
-//  }
+  def create(): Action[JsValue] = Action.async(parse.json) = { implicit request =>
+    repositoryService.create(request).map {
+      case Right(book) => Created(Json.toJson(book))
+      case Left(error) => BadRequest(Json.toJson(error))
+    }
+  }
+
+  def read(id: String): Action[AnyContent] = Action.async { implicit request =>
+    repositoryService.read(id).map {
+      case Right(book: UserModel) => Ok(Json.toJson(book))
+      case Left(error) => BadRequest(Json.toJson(error))
+    }
+  }
+
+  def update(id: String): Action[JsValue] = Action.async(parse.json) { implicit request =>
+    repositoryService.update(id, request).map {
+      case Right(book) => Accepted(Json.toJson(book))
+      case Left(error) => BadRequest(Json.toJson(error))
+    }
+  }
+
+  def delete(id: String): Action[AnyContent] = Action.async { implicit request =>
+    repositoryService.delete(id: String).map {
+      case Right(message) => Accepted(Json.toJson(message))
+      case Left(error) => BadRequest(Json.toJson(error))
+    }
+  }
 }
